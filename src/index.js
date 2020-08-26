158d5d2473090b5c2eba00f5d97d2478fe93b4e0
daninkc <41238045+daninkc@users.noreply.github.com>
2020-08-26 11:14:18 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
-import styles from '/Users/Daniela/Desktop/React JS/Projects/my-app/src/bootstrap/css/bootstrap.css';
-import functioning from '/Users/Daniela/Desktop/React JS/Projects/my-app/src/bootstrap/js/bootstrap.js';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 
 ReactDOM.render(
